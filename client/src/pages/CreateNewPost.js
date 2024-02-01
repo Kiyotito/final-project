9a66b196298ce933d53b9d313fcb3eb0a9a4821c
Koichi Sato <koichisatocarrillo@gmail.com>
2024-01-31 22:25:00 -0400

    Styling Changes

--- client/src/pages/CreateNewPost.js
+++ client/src/pages/CreateNewPost.js
@@ -4,121 +4,149 @@ import 'react-quill/dist/quill.snow.css';
 import styled from "styled-components";
 import { useContext } from "react";
 import { UserContext } from "../context/UserContext";
+import { useNavigate } from "react-router-dom";
+
+
+
+
+
 
 
 
 
 
 const modules = {
-    toolbar: [
-        [{ 'header': [1, 2, false] }],
-        ['bold', 'italic', 'underline','strike', 'blockquote'],
-        [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-        ['link', 'image'],
-        ['clean']
-      ] 
+   toolbar: [
+       [{ 'header': [1, 2, false] }],
+       ['bold', 'italic', 'underline','strike', 'blockquote'],
+       [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+       ['link', 'image'],
+       ['clean']
+     ]
 }
 const formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
+   'header',
+   'bold', 'italic', 'underline', 'strike', 'blockquote',
+   'list', 'bullet', 'indent',
+   'link', 'image'
 ]
 
 
 
+
+
+
 const CreateNewPost = () => {
-    const [title,setTitle]= useState('');
-    const [summary,setSummary]= useState('');
-    const [content,setContent]= useState('');
-    const [imgURL,setImgURL]= useState('');
-    const [formData, setFormData] = useState({});
-    const {user} =useContext(UserContext);
-    const userName = user.name;
-    const date = new Date()
-    const dateString = date.toDateString();
+   const [formData, setFormData] = useState({});
+   const {user} =useContext(UserContext);
+   const userName = user.name;
+   const date = new Date()
+   const dateString = date.toDateString();
+   const navigate = useNavigate();
+
+
 
 
 const handleChange = (id,value)=> {
-    setFormData({
-        ...formData,
-        [id]:value,
-    })
+   setFormData({
+       ...formData,
+       [id]:value,
+   })
 }
+
 
 const handleTextChange = (text,value)=> {
-    setFormData({
-        ...formData,
-        text:value,
-        author: userName,
-        date: dateString, 
-    })
+   setFormData({
+       ...formData,
+       text:value,
+       author: userName,
+       date: dateString,
+   })
 }
 
-    const createNewPost = async (ev) =>{
-        ev.preventDefault();
-        /*setFormData({
-            ...formData,
-            author: userName,
-            date: dateString, 
-        })*/
-        console.log(formData);
-        const response = await fetch('/create-post',{
-            method: 'POST',
-            headers:{
-                Accept: "application/json",
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({content:formData}),
-        })
-    }
 
-   
+   const createNewPost = async (ev) =>{
+       ev.preventDefault();
+       console.log(formData);
+       const response = await fetch('/create-post',{
+           method: 'POST',
+           headers:{
+               Accept: "application/json",
+               "Content-Type": "application/json",
+           },
+           body: JSON.stringify({content:formData}),
+       })
+       navigate(`/`)
+   }
 
-    return(
-        <Form onSubmit={createNewPost}>
-            <label htmlFor="Title">Title</label>
-            <Input type="title"
-            id= "title"
-            name="postContent" 
-            placeholder={'Title'}
-            onChange={ev=>handleChange(ev.target.id, ev.target.value)}/>
-            <label htmlFor="Summary">Summary</label>
-            <Input type="summary" 
-            id= "summary"
-            name="postContent" 
-            placeholder={'Summary'}
-            onChange={ev=>handleChange(ev.target.id, ev.target.value)}/>
-            <label htmlFor="imgURL">ImageURL</label>
-            <Input type="text"
-            id= "imgURL"
-            name="postContent"  
-            placeholder={'Image URL'}
-            onChange={ev=>handleChange(ev.target.id, ev.target.value)}/>
-            <ReactQuill 
-            id= "content"
-            name="postContent" 
-            //value ={content} 
-            onChange={newValue=>handleTextChange("text", newValue)}
-            modules={modules} formats={formats}/>
-            <Button>Create Post</Button>
-        </Form>
-    )
+
+ 
+
+
+   return(
+       <Form onSubmit={createNewPost}>
+           <label htmlFor="Title">Title</label>
+           <Input type="title"
+           id= "title"
+           name="postContent"
+           placeholder={'Title'}
+           onChange={ev=>handleChange(ev.target.id, ev.target.value)}/>
+           <label htmlFor="Summary">Summary</label>
+           <Input type="summary"
+           id= "summary"
+           name="postContent"
+           placeholder={'Summary'}
+           onChange={ev=>handleChange(ev.target.id, ev.target.value)}/>
+           <label htmlFor="imgURL">ImageURL</label>
+           <Input type="text"
+           id= "imgURL"
+           name="postContent" 
+           placeholder={'Image URL'}
+           onChange={ev=>handleChange(ev.target.id, ev.target.value)}/>
+           <Editor
+           id= "content"
+           name="postContent"
+           //value ={content}
+           onChange={newValue=>handleTextChange("text", newValue)}
+           modules={modules} formats={formats}/>
+           <Button>Create Post</Button>
+       </Form>
+   )
+
 
 }
+
 
 const Form = styled.form`
 display: flex;
 flex-direction: column;
-gap: 10px;
+gap: 20px;
 justify-content: center;
+align-items: center;
 `
 const Input = styled.input`
 height: 25px;
 border-radius: 5px;
+width: 50vw;
 `
-const Button = styled.button`
-margin-top: 10px;
+const Button =styled.button`
+border-radius: 10px;
+padding: 10px 20px;
+text-decoration: none;
+border: none;
+background-color: #F9A873;
+font-weight: 900;
+width: 20vw;
+&:hover{
+background-color: #F67C2D;
+}`
+
+
+const Editor = styled(ReactQuill)`
+background-color: white;
+width: 50vw;
+margin-bottom: 20px;
 `
+
 
 export default CreateNewPost;
--- client/src/pages/IndividualPost.js
+++ client/src/pages/IndividualPost.js
@@ -5,62 +5,67 @@ import styled from "styled-components";
 import { DiscussionEmbed } from 'disqus-react';
 
 
+
+
 const IndividualPost = () => {
-  const [post, setPost] = useState();
-  const { id } = useParams();
-  const [comments, setComments] = useState();
-  //const [description, setDescription] = useState();
+ const [post, setPost] = useState();
+ const { id } = useParams();
+ const [comments, setComments] = useState();
+ //const [description, setDescription] = useState();
+
 
 useEffect(()=>{
- fetch(`/get-a-post/${id}`)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        throw new Error("Could not retrieve post!");
-      }
-    })
-    .then((data) => {
-      setPost(data.data);
-      //setDescription = post.content.text;
-    })
-    .catch((error) => {
-      console.error("Invalid Data Received:", error);
-    });
+fetch(`/get-a-post/${id}`)
+   .then((response) => {
+     if (response.ok) {
+       return response.json();
+     } else {
+       throw new Error("Could not retrieve post!");
+     }
+   })
+   .then((data) => {
+     setPost(data.data);
+     //setDescription = post.content.text;
+   })
+   .catch((error) => {
+     console.error("Invalid Data Received:", error);
+   });
 },[])
 
-  return ( 
-  <div>
-  {
-    post ?
-    <>
-    <PostPage>
-    <Img src={post.content.imgURL} alt=""/>
-    <ContentSection>
-    <Title>{post.content.title}</Title>
-    <Summary>{post.content.summary}</Summary>
-    <div dangerouslySetInnerHTML={{__html: post.content.text}}/>
-    </ContentSection>
-    
-    </PostPage>
-    <DiscussionEmbed
-    shortname='example'
-    config={
-        {
-            url: `http://localhost:3000/post/${id}`,
-            identifier: `${id}`,
-            title: `${post.content.title}`,
-            language: 'eng' //e.g. for Traditional Chinese (Taiwan)	
-        }
-    }
-    />
-    </>
-    :
-    <div>Loading</div>
-  }
-  </div>
-  )
+
+ return (
+ <div>
+ {
+   post ?
+   <>
+   <PostPage>
+   <Img src={post.content.imgURL} alt=""/>
+   <ContentSection>
+   <Title>{post.content.title}</Title>
+   <Summary>{post.content.summary}</Summary>
+   <MainText dangerouslySetInnerHTML={{__html: post.content.text}}/>
+   </ContentSection>
+  
+   </PostPage>
+   <DiscussionEmbed
+   shortname='example'
+   config={
+       {
+           url: `http://localhost:3000/post/${id}`,
+           identifier: `${id}`,
+           title: `${post.content.title}`,
+           language: 'eng' //e.g. for Traditional Chinese (Taiwan)
+       }
+   }
+   />
+   </>
+   :
+   <div>Loading</div>
+ }
+ </div>
+ )
 };
+
 
 const Img = styled.img`
 max-width: 250px;
@@ -68,6 +73,7 @@ height: auto;
 border-radius: 25px;
 margin: 20px;
 `
+
 
 const PostPage = styled.div`
 display: flex;
@@ -85,13 +91,20 @@ flex-direction: column;
 align-items: center;
 `
 
+
 const Title = styled.h1`
- font-size: 2rem;
- margin-bottom: 10px;
+font-size: 2rem;
+margin-bottom: 10px;
 `;
 
+
 const Summary = styled.h2`
- font-size: 1rem;
- margin-bottom: 10px;
+font-size: 1rem;
+margin-bottom: 10px;
+`;
+
+
+const MainText = styled.div`
+margin-top: 25px;
 `;
 export default IndividualPost;
